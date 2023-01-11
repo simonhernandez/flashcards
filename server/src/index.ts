be551a23c9beb6cb669a4d9cf18b7561a74d4954
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2023-01-11 01:20:31 -0400

    added get decks endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ config();
 
 import express, { Response, Request } from "express";
 import mongoose from "mongoose";
-import cors from 'cors';
+import cors from "cors";
 
 import Deck from "./models/Deck";
 
@@ -11,13 +11,21 @@ const PORT = 5000;
 
 const app = express();
 
-app.use(cors({
-    origin: '*' // allows any origin to have access to your API
-}))
+app.use(
+  cors({
+    origin: "*", // allows any origin to have access to your API
+  })
+);
 app.use(express.json()); // everytime someone makes requests to our API, it is going to run whatever function inside of 'use'. express.json() parses JSON Post Requests
 
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+  console.log(decks);
+  res.json(decks);
+});
+
 app.post("/decks", async (req: Request, res: Response) => {
-    console.log(req.body)
+  console.log(req.body);
   const newDeck = new Deck({
     title: req.body.title,
   });
@@ -34,11 +42,7 @@ app.get("/tgif", (req: Request, res: Response) => {
   res.send("tgif");
 });
 
-mongoose
-  .connect(
-    process.env.MONGO_URL!
-  )
-  .then(() => {
-    console.log(`listening on port ${PORT}`);
-    app.listen(PORT);
-  });
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`listening on port ${PORT}`);
+  app.listen(PORT);
+});
